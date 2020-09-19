cfc1a80f261a2a5feea6eba990640a3ef3d0ffe0
Aishwarya2407 <52240784+Aishwarya2407@users.noreply.github.com>
2020-09-19 20:29:49 +0530

    Create Addition through Strings.java

---
+++ Addition through Strings.java
@@ -0,0 +1,15 @@
+# wipro-PRP
+Answers for wipro PRP programs.
+import java.util."
+
+import java.math.Bigdecimal;
+
+class UsermainCode
+{
+public String addNumberStrings(string inputi,String Input2)
+{
+BigDecimal a=new BigDecimal(input)
+
+BigDecimal b=new BigDecimal(input2);
+return String.valueof(a.add(b));}
+}
